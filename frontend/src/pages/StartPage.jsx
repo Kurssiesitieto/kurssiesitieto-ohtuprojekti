f0be55e22402500d2af94637185de600f24e8913
sampsaoinonen <sampsa.oinonen@gmail.com>
2024-08-28 15:30:10 +0300

    Update text in StartPage

--- frontend/src/pages/StartPage.jsx
+++ frontend/src/pages/StartPage.jsx
@@ -15,8 +15,7 @@ const StartPage = () => {
       </div>
       <div className="content">
         <p>Tämä sovellus näyttää tarvittavat kurssiesitiedot tietyille tutkinto-ohjelmille Helsingin yliopistossa.</p>
-        <p>Käyttäjäopas löytyy <a href="https://github.com/Kurssiesitieto/kurssiesitieto-ohtuprojekti/blob/main/documentation/user-guide.md">täältä</a></p>
-        <p>Käyttäjäoppaasta <a href="https://github.com/Kurssiesitieto/kurssiesitieto-ohtuprojekti/blob/main/documentation/user-guide.md">löytyy</a> ohjeet uuden tutkinnon lisäämiseen</p>      
+        <p>Käyttäjäopas löytyy <a href="https://github.com/Kurssiesitieto/kurssiesitieto-ohtuprojekti/blob/main/documentation/user-guide.md">täältä</a></p>        
         <p></p>
         <p>Tämä sovellus on luotu Ohjelmistotuotanto-kurssin projektityönä Helsingin yliopistolle.</p>
         <p>Lähdekoodi löytyy <a href="https://github.com/Kurssiesitieto/kurssiesitieto-ohtuprojekti">täältä</a></p>
